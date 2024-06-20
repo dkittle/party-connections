0b8404bcce4b1767c9409f95c67c032c034b0d97
Don Kittle <don@kittle.ca>
2024-06-20 15:09:59 -0400

    Decouple creation and storage of backstory summary from the flow that collects player character collection changes.

--- src/main/kotlin/ca/kittle/backstory/BackstorySummary.kt
+++ src/main/kotlin/ca/kittle/backstory/BackstorySummary.kt
@@ -2,10 +2,13 @@ package ca.kittle.backstory
 
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import java.util.UUID
 
+@Serializable
 data class BackstorySummary(
     val summary: String,
+    val pcId: String,
     @Contextual @SerialName("_id")
     val id: String = UUID.randomUUID().toString(),
 )
--- src/main/kotlin/ca/kittle/plugins/ProcessPCUpdates.kt
+++ src/main/kotlin/ca/kittle/plugins/ProcessPCUpdates.kt
@@ -4,6 +4,7 @@ import ca.kittle.backstory.BackstorySummary
 import ca.kittle.db.models.BackstorySummaryEntity
 import ca.kittle.db.models.PlayerCharacterEntity
 import ca.kittle.db.models.toDocument
+import ca.kittle.pc.models.PlayerCharacter
 import ca.kittle.util.json
 import com.mongodb.client.MongoDatabase
 import com.mongodb.client.model.Aggregates
@@ -16,9 +17,12 @@ import io.ktor.client.request.setBody
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import kotlinx.serialization.encodeToString
 import mu.KotlinLogging
 
@@ -28,8 +32,13 @@ context(MongoDatabase, LlmConnection)
 fun Application.processPCUpdates() {
     launch {
         logger.info { "Starting player character update flow" }
-        consumePlayerCharacterUpdates()
-        // this@MongoDatabase, this@LlmConnection
+        consumePlayerCharacterUpdates().flowOn(Dispatchers.IO).collect { pc ->
+            val backstory = pc.backstory
+            logger.info { "Summarizing ${pc.name}'s backstory" }
+            val summary = generateBackstorySummary(backstory)
+            logger.info { "Storing ${pc.name}'s backstory" }
+            storeBackstorySummary(summary, pc.id)
+        }
     }
 }
 
@@ -37,34 +46,30 @@ private const val QUOTES = "\"\"\""
 private const val SUMMARY_PROMPT = """Please summarize this dungeons and dragons character backstory in bullet point form. Do not leave out any important points.\n"""
 
 context(MongoDatabase, LlmConnection)
-suspend fun consumePlayerCharacterUpdates(
-//    connection: MongoDatabase,
-//    llmConnection: LlmConnection,
-) {
-    val pipeline =
-        listOf(
-            Aggregates.match(
-                Filters.`in`("operationType", mutableListOf("insert", "update", "delete")),
-            ),
-        )
-    val collection = this@MongoDatabase.getCollection(PlayerCharacterEntity.COLLECTION_NAME)
-    val changeStreamFlow =
-        collection.watch(pipeline)
-            .fullDocument(FullDocument.DEFAULT).asFlow().flowOn(Dispatchers.IO)
-    logger.info { "Flow started" }
-    changeStreamFlow.collect { event ->
-        if (event.operationType != OperationType.DELETE) {
-            event.fullDocument?.let { doc ->
-                val operation = "${event.operationType.name}${if (event.operationType.name.endsWith("e")) "d" else "ed"}"
-                val pc = PlayerCharacterEntity.fromDocument(doc)
-                logger.info { "${pc.name} $operation" }
-                val backstory = pc.backstory
-                val summary = generateBackstorySummary(backstory)
-                storeBackstorySummary(summary)
+suspend fun consumePlayerCharacterUpdates(): Flow<PlayerCharacter> =
+    flow {
+        val pipeline =
+            listOf(
+                Aggregates.match(
+                    Filters.`in`("operationType", mutableListOf("insert", "update", "delete")),
+                ),
+            )
+        val collection = this@MongoDatabase.getCollection(PlayerCharacterEntity.COLLECTION_NAME)
+        val changeStreamFlow =
+            collection.watch(pipeline)
+                .fullDocument(FullDocument.DEFAULT).asFlow().flowOn(Dispatchers.IO)
+        logger.info { "Flow started" }
+        changeStreamFlow.collect { event ->
+            if (event.operationType != OperationType.DELETE) {
+                event.fullDocument?.let { doc ->
+                    val operation = "${event.operationType.name.lowercase()}${if (event.operationType.name.endsWith("e")) "d" else "ed"}"
+                    val pc = PlayerCharacterEntity.fromDocument(doc)
+                    logger.info { "${pc.name} $operation" }
+                    emit(pc)
+                }
             }
         }
     }
-}
 
 context(LlmConnection)
 suspend fun generateBackstorySummary(
@@ -72,7 +77,6 @@ suspend fun generateBackstorySummary(
     backstory: String,
 ): String {
     val prompt = "$QUOTES $SUMMARY_PROMPT $backstory $QUOTES"
-    logger.info { json.encodeToString(GenerateRequest(prompt)) }
     val llmResponse =
         this@LlmConnection.llmClient.post("${this@LlmConnection.llmUrl}generate") {
             setBody(
@@ -94,9 +98,14 @@ suspend fun generateBackstorySummary(
 
 context(MongoDatabase)
 suspend fun storeBackstorySummary(
-//    connection: MongoDatabase,
     summary: String,
-) {
-    val collection = this@MongoDatabase.getCollection(BackstorySummaryEntity.COLLECTION_NAME)
-    collection.insertOne(BackstorySummary(summary).toDocument())
-}
+    pcId: String,
+): Result<Boolean> =
+    withContext(Dispatchers.IO) {
+        runCatching {
+            val collection = this@MongoDatabase.getCollection(BackstorySummaryEntity.COLLECTION_NAME)
+            collection.insertOne(BackstorySummary(summary, pcId).toDocument()).wasAcknowledged()
+        }.onFailure {
+            logger.error(it) { "Failed to store backstory summary" }
+        }
+    }
